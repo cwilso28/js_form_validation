d150e7914dae516bc79ea22ee8ebc15fce0d5b38
Chris <47066922+cwilso28@users.noreply.github.com>
2025-09-11 20:02:31 -0500

    Add email input event listener
    This initializes the event listener that will evaluate the format of the
    input as the user types it.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./styles.css"
+
+let email = document.getElementById("email");
+
+email.addEventListener("input", (event) => {
+    if (email.validitiy.valid) {
+        emailError.textContent = "";
+        emailError.className = "error";
+    }
+
+    else {
+        showError();
+    }
+})
