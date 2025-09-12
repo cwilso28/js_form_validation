7f4d233d0e8e17e81035da3585b05bcfedba1f13
Chris <47066922+cwilso28@users.noreply.github.com>
2025-09-11 20:07:09 -0500

    Initialize email error function

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import "./styles.css"
 let email = document.getElementById("email");
 
 email.addEventListener("input", (event) => {
-    if (email.validitiy.valid) {
+    if (email.validity.valid) {
         emailError.textContent = "";
         emailError.className = "error";
     }
 
     else {
-        showError();
+        showEmailError();
     }
 })
+
+function showEmailError() {
+
+}
